cd2cc7dc220fefe66fcad82335afd6c94a2f52d7
Jeffrey Rios <jeffrey.rios@correounivalle.edu.co>
2019-09-28 22:36:58 -0500

    scripts bd agregados

--- src/db/database.sql
+++ src/db/database.sql
@@ -3,6 +3,9 @@ USE api_rest_nodejs;
 
 CREATE TABLE employee(
 	id INT NOT NULL AUTO_INCREMENT,
-    nombre
-
+    name VARCHAR(45) DEFAULT NULL,
+    salary int(11) DEFAULT NULL,
+    PRIMARY KEY(id)
 );
+
+DESCRIBE employee;
