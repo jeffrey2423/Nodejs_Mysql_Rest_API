2c9053b2a1eb6af9e264afa34ba7bcf5f101fcd2
Jeffrey Rios <jeffrey.rios@correounivalle.edu.co>
2019-09-28 23:39:24 -0500

    scripts bd terminados

---
+++ db/database.sql
@@ -0,0 +1,17 @@
+/*CREATE DATABASE api_rest_nodejs;
+USE api_rest_nodejs;
+
+CREATE TABLE employee(
+	id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) DEFAULT NULL,
+    salary int(11) DEFAULT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO employee VALUES
+(1, 'PRUEBA', 20000),
+(2, 'PRUEBA 2', 30000),
+(3, 'PRUEBA 3', 40000),
+(4, 'PRUEBA 4', 50000);
+
+DESCRIBE employee;*/
