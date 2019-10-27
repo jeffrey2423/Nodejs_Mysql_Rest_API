667ce31e5bd9cd78ff0208c6ad998bb7e1e5e96b
Jeffrey Rios <jeffrey.rios@correounivalle.edu.co>
2019-10-26 21:47:46 -0500

    variables de entorno configuradas

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'api_rest_nodejs',
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.DATABASE,
   multipleStatements: true
 });
 
